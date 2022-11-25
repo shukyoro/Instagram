6a0c4f1105c81b8d82795a6de6fb013aee4e74c2
shuuto.hirota <dgs.hirota@gmail.com>
2022-11-25 16:58:16 +0900

    Instagramアプリ作成

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 廣田秀人 on 2022/11/25.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
